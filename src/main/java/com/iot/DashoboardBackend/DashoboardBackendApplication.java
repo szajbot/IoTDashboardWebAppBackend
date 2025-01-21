c24c93fb5a9d82a1d9222880b606c644a2ef9b51
waszkowitobi <tobiasz.waszkowiak@wat.edu.pl>
2025-01-21 11:47:47 +0100

    Initialize project

---
+++ src/main/java/com/iot/DashoboardBackend/DashoboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iot.DashoboardBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashoboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashoboardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iot/DashoboardBackend/DashoboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.DashoboardBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashoboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
